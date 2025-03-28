bc7ec0f0f685991c02adb65d1e555e6fce223ef7
ZheJin0527 <Zjwu0527>
2025-03-28 16:56:12 +0800

    testing264

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="zh">
 <head>
+    <link rel="icon" type="image/png" href="images/images/logo.png">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>KUNZZ HOLDINGS</title>
