5389924bf93cb42ab09cf903b3c94817799beb28
ZheJin0527 <Zjwu0527>
2025-03-28 14:03:03 +0800

    testing249

--- index.php
+++ index.php
@@ -361,7 +361,7 @@
         .timeline-item:nth-child(odd) .year,
         .timeline-item:nth-child(odd) .timeline-text {
             position: relative;
-            top: -125px; /* 让年份和段落都靠近时间轴 */
+            top: -110px; /* 让年份和段落都靠近时间轴 */
             font-weight: bold;
         }
 
