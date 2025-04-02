9675afd3649f08872c772d1748ffd9162598c8e0
ZheJin0527 <Zjwu0527>
2025-04-02 10:06:14 +0800

    testing361

--- index.php
+++ index.php
@@ -64,7 +64,7 @@
 
         /* Logo 图片 */
         .logo-img {
-            height: clamp(25px, 4vw, 65px); /* 最大高度调整为 65px */
+            height: clamp(25px, 4.2vw, 65px); /* 最大高度调整为 65px */
             max-height: 75px;
         }
 
