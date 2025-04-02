95b7534a348bde4ca1ccc363a6bd4f992e45114c
ZheJin0527 <Zjwu0527>
2025-04-02 14:55:39 +0800

    testing436

--- index.php
+++ index.php
@@ -134,8 +134,8 @@
 
         /* 翻译按钮中的 Logo 图片样式 */
         .translate-btn .translate-logo-img {
-            width: clamp(35px, 5vw, 100px);  /* Logo 尺寸根据按钮大小调整 */
-            height: clamp(35px, 5vw, 100px); /* 设置 Logo 高度 */
+            width: clamp(35px, 5vw, 90px);  /* Logo 尺寸根据按钮大小调整 */
+            height: clamp(35px, 5vw, 90px); /* 设置 Logo 高度 */
             object-fit: contain;  /* 保证 logo 按比例显示，避免变形 */
             margin-right: -10px; /* 让 logo 贴近文字 */
         }
