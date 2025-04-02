f000cd2c11542cd1e1e9d941e979b7ade7d7b44b
ZheJin0527 <Zjwu0527>
2025-04-02 09:40:13 +0800

    testing351

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
             justify-content: space-between;
             align-items: center;
             width: 100%;
-            padding: 0 5%;
+            padding: 0;
             position: relative;
         }
 
