13848e465ea9324be8b993ae1cf89e74ea183995
ZheJin0527 <Zjwu0527>
2025-04-03 11:36:09 +0800

    testing508

--- index.php
+++ index.php
@@ -565,7 +565,7 @@
             height: auto;
             object-fit: cover;
             border-radius: 10px;
-            z-index: -1; /* 让虚线在最底层，隐藏到照片后面 */
+            z-index: 1; /* 让虚线在最底层，隐藏到照片后面 */
         }
 
         /* 小屏幕（768px 以下），进一步缩小 */
