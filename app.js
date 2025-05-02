85fd361a182d20b32414418a33473e24a4a59627
ZheJin0527 <Zjwu0527>
2025-05-02 11:18:09 +0800

    134

--- app.js
+++ app.js
@@ -202,4 +202,33 @@ function toggleDetail(el) {
   }
 }
 
+new Swiper('.environment-wrapper', {
+  loop: true,
+  spaceBetween: 30,
+
+  // Pagination bullets
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+      0: {
+          slidesPerView: 1
+      },
+      768: {
+          slidesPerView: 2
+      },
+      1024: {
+          slidesPerView: 3
+      },
+  }
+});
 
