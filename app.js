ff61e3dea0fda8eb1aa8838cbcba6f5ba0156854
ZheJin0527 <Zjwu0527>
2025-05-06 14:45:27 +0800

    185

--- app.js
+++ app.js
@@ -226,3 +226,14 @@ function toggleDetail(el) {
     }
   }
 }
+
+// 清空表单：如果是从 success.html?from=form 回来的
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("jobApplicationForm");
+
+  // 检查是否是从 success.html 页面跳转回来的
+  const referrer = document.referrer;
+  if (referrer.includes("success.html?from=form") && form) {
+    form.reset();
+  }
+});
