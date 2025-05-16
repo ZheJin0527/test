6f650adbcaa6a81f332f631bb38fe9a3dbe1b75d
ZheJin0527 <Zjwu0527>
2025-05-16 15:21:57 +0800

    491

--- edit_profile.php
+++ edit_profile.php
@@ -65,68 +65,69 @@ $conn->close();
     <link rel="stylesheet" href="style.css" />
 </head>
 <body>
+<div class="edit-section">
     <h2>编辑个人资料</h2>
 
     <?php if ($error): ?>
-        <p style="color: red;"><?php echo htmlspecialchars($error); ?></p>
+        <p style="color: red;"><?= htmlspecialchars($error); ?></p>
     <?php endif; ?>
+
     <?php if ($success): ?>
-        <p style="color: green;"><?php echo htmlspecialchars($success); ?></p>
+        <p style="color: green;"><?= htmlspecialchars($success); ?></p>
     <?php endif; ?>
 
     <form method="POST" action="edit_profile.php">
-        <fieldset>
-            <legend>联系资料</legend>
-            <label>用户名:</label><br>
-            <input type="text" name="username" value="<?php echo htmlspecialchars($user['username']); ?>" required><br><br>
+        <div class="section-title">联系资料</div>
 
-            <label>邮箱（不可修改）:</label><br>
-            <input type="email" value="<?php echo htmlspecialchars($user['email']); ?>" readonly><br><br>
+        <label>用户名:</label>
+        <input type="text" name="username" value="<?= htmlspecialchars($user['username']); ?>" required>
 
-            <label>联络号码:</label><br>
-            <input type="text" name="phone" value="<?php echo htmlspecialchars($user['phone_number']); ?>"><br><br>
+        <label>邮箱（不可修改）:</label>
+        <input type="email" value="<?= htmlspecialchars($user['email']); ?>" readonly>
 
-            <label>银行名称:</label><br>
-            <input type="text" name="bank_name" value="<?php echo htmlspecialchars($user['bank_name']); ?>"><br><br>
+        <label>联络号码:</label>
+        <input type="text" name="phone" value="<?= htmlspecialchars($user['phone_number']); ?>">
 
-            <label>银行账号:</label><br>
-            <input type="text" name="bank_account" value="<?php echo htmlspecialchars($user['bank_account']); ?>"><br><br>
+        <label>银行名称:</label>
+        <input type="text" name="bank_name" value="<?= htmlspecialchars($user['bank_name']); ?>">
 
-            <label>户籍地址:</label><br>
-            <input type="text" name="home_address" value="<?php echo htmlspecialchars($user['home_address']); ?>"><br><br>
+        <label>银行账号:</label>
+        <input type="text" name="bank_account" value="<?= htmlspecialchars($user['bank_account']); ?>">
 
-            <label>现居地址:</label><br>
-            <input type="text" name="current_address" value="<?php echo htmlspecialchars($user['current_address']); ?>"><br><br>
+        <label>家庭地址:</label>
+        <input type="text" name="home_address" value="<?= htmlspecialchars($user['home_address']); ?>">
 
-            <label>城市:</label><br>
-            <input type="text" name="city" value="<?php echo htmlspecialchars($user['city']); ?>"><br><br>
+        <label>现居地址:</label>
+        <input type="text" name="current_address" value="<?= htmlspecialchars($user['current_address']); ?>">
 
-            <label>州属:</label><br>
-            <input type="text" name="state" value="<?php echo htmlspecialchars($user['state']); ?>"><br><br>
+        <label>城市:</label>
+        <input type="text" name="city" value="<?= htmlspecialchars($user['city']); ?>">
 
-            <label>邮编:</label><br>
-            <input type="text" name="postcode" value="<?php echo htmlspecialchars($user['postcode']); ?>"><br><br>
-        </fieldset>
+        <label>州属:</label>
+        <input type="text" name="state" value="<?= htmlspecialchars($user['state']); ?>">
 
-        <fieldset>
-            <legend>个人资料</legend>
-            <label>职位（不可修改）:</label><br>
-            <input type="text" value="<?php echo htmlspecialchars($user['position']); ?>" readonly><br><br>
+        <label>邮编:</label>
+        <input type="text" name="postcode" value="<?= htmlspecialchars($user['postcode']); ?>">
 
-            <label>出生日期:</label><br>
-            <input type="date" name="date_of_birth" value="<?php echo htmlspecialchars($user['date_of_birth']); ?>"><br><br>
+        <div class="section-title">个人资料</div>
 
-            <label>性别:</label><br>
-            <select name="gender">
-                <option value="">请选择</option>
-                <option value="male" <?php if ($user['gender'] == 'male') echo 'selected'; ?>>男</option>
-                <option value="female" <?php if ($user['gender'] == 'female') echo 'selected'; ?>>女</option>
-                <option value="other" <?php if ($user['gender'] == 'other') echo 'selected'; ?>>其他</option>
-            </select><br><br>
-        </fieldset>
+        <label>职位（不可修改）:</label>
+        <input type="text" value="<?= htmlspecialchars($user['position']); ?>" readonly>
+
+        <label>出生日期:</label>
+        <input type="date" name="date_of_birth" value="<?= htmlspecialchars($user['date_of_birth']); ?>">
+
+        <label>性别:</label>
+        <select name="gender">
+            <option value="">请选择</option>
+            <option value="男" <?= $user['gender'] === '男' ? 'selected' : '' ?>>男</option>
+            <option value="女" <?= $user['gender'] === '女' ? 'selected' : '' ?>>女</option>
+            <option value="其他" <?= $user['gender'] === '其他' ? 'selected' : '' ?>>其他</option>
+        </select>
 
         <button type="submit">保存修改</button>
-        <a href="dashboard.php" style="margin-left: 15px;">返回</a>
+        <a href="dashboard.php">返回</a>
     </form>
+</div>
 </body>
 </html>
