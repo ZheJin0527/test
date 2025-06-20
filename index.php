60e807d9234c36006c1ed3a16a935a3fa26a5ce2
ZheJin0527 <Zjwu0527>
2025-06-20 14:55:43 +0800

    1138

--- index.php
+++ index.php
@@ -66,12 +66,11 @@ if (isset($_SESSION['user_id']) || (isset($_COOKIE['user_id']) && isset($_COOKIE
   <div class="swiper-wrapper">
 
   <div class="swiper-slide">
-  
-<section class="home">
+  <section class="home">
     <div class="home-content hidden animate-on-scroll">
-      <h1 class="scale-fade-in title-text-scan-enhanced" data-text="让空间温暖 . 让团队闪光">让空间温暖 <span style="font-size: 1.5em;">.</span> 让团队闪光</h1>
+      <h1 class="scale-fade-in title-light-scan">让空间温暖 <span style="font-size: 1.5em;">.</span> 让团队闪光</h1>
       <div class="decor-line scale-fade-in"></div>
-      <p class="scale-fade-in">
+      <p class="scale-fade-in light-scan-luxury">
         我们用细节构建舒适的氛围，在积极的文化中滋养每一份热情与专注。<br />
         我们相信，高效源于信任，创新源于自由。一支有温度的团队，<br />
         才能创造持续的价值，向着行业标杆的方向，稳步前行。
@@ -376,7 +375,7 @@ updatePageIndicator(0);
       void document.querySelector('.home-content').offsetWidth;
 
       // 添加动画类（如果你的 fade-in-up 是靠 JavaScript 加载）
-      document.querySelector('.home-content h1').classList.add('scale-fade-in', 'title-text-scan-enhanced');
+      document.querySelector('.home-content h1').classList.add('scale-fade-in');
       document.querySelector('.home-content p').classList.add('scale-fade-in');
     };
   });
