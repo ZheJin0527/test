fe50e5962ee456ce5c83b5dc73973773cd6c922d
ZheJin0527 <Zjwu0527>
2025-07-23 11:57:54 +0800

    1930

--- en/index.php
+++ en/index.php
@@ -58,8 +58,8 @@ if (isset($_SESSION['user_id']) || (isset($_COOKIE['user_id']) && isset($_COOKIE
     <div class="language-switch">
       <a href="#" class="lang" id="languageBtn">EN | CN</a>
         <div class="language-dropdown-menu" id="languageDropdownMenu">
-          <a href="#" class="language-dropdown-item" data-lang="en">英文</a>
-          <a href="#" class="language-dropdown-item" data-lang="cn">中文</a>
+          <a href="/en/" class="language-dropdown-item" data-lang="en">英文</a>
+          <a href="/" class="language-dropdown-item" data-lang="cn">中文</a>
         </div>
       </div>
 
