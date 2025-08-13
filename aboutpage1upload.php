f37b2ec1167d66cc7940e47d062b76859e551e90
ZheJin0527 <Zjwu0527>
2025-08-13 14:40:19 +0800

    2683

--- aboutpage1upload.php
+++ aboutpage1upload.php
@@ -83,7 +83,7 @@ if (file_exists('media_config.json')) {
         
         body {
             font-family: 'Inter', sans-serif;
-            background: linear-gradient(135deg, #1e3c72 0%, #2a5298 100%);
+            background: linear-gradient(135deg, #ffffffff 0%, #f3ebe0ff 100%);
             min-height: 100vh;
             padding: 20px;
         }
