76287c6c2ce7af5560d24892bd73fd5a16fc4267
ZheJin0527 <Zjwu0527>
2025-08-25 15:44:36 +0800

    3170

--- stocklist.php
+++ stocklist.php
@@ -868,8 +868,8 @@
             // 添加总计行
             tableRows += `
                 <tr class="total-row">
-                    <td colspan="6" class="text-right" style="font-size: 16px; padding-right: 15px; text-align: right; background: white;">总计:</td>
-                    <td class="price-cell positive-value" style="font-size: 16px; background: white;">
+                    <td colspan="6" class="text-right" style="font-size: 16px; padding-right: 15px; text-align: right;">总计:</td>
+                    <td class="price-cell positive-value" style="font-size: 16px;">
                         <div class="currency-display">
                             <span class="currency-symbol">RM</span>
                             <span class="currency-amount">${formatCurrency(totalValue)}</span>
