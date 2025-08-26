cb113478c4b2f019cfe84188df3b75364d4e821a
ZheJin0527 <Zjwu0527>
2025-08-26 16:02:32 +0800

    3191

--- stockedit.php
+++ stockedit.php
@@ -1024,7 +1024,7 @@
     <div class="container">
         <div class="header">
             <div>
-                <h1>库存进出货管理系统</h1>
+                <h1>中央库存进出货管理系统</h1>
             </div>
             <div class="controls">
                 <div class="page-selector" onclick="togglePageDropdown()">
