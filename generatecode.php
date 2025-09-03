ed4aa650c724e17dc25f00a904fce6a23851db4a
ZheJin0527 <Zjwu0527>
2025-09-03 12:06:25 +0800

    3465

--- generatecode.php
+++ generatecode.php
@@ -553,7 +553,9 @@
                 month: '2-digit',
                 day: '2-digit',
                 hour: '2-digit',
-                minute: '2-digit'
+                minute: '2-digit',
+                hour12: false,
+                timeZone: 'Asia/Shanghai'
             });
         }
 
