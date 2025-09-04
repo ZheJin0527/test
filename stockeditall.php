7b246ec0c25374c94878c5b3ce1b66d5e05b2487
unknown <darrellchua118@gmail.com>
2025-09-04 17:35:23 +0800

    D052

--- stockeditall.php
+++ stockeditall.php
@@ -4263,7 +4263,7 @@
                     yPosition = height - 186; // J1模板的起始Y坐标
                     lineHeight = 16.01; // J1模板的行高
                 } else { // j2
-                    yPosition = height - 301; // J2模板的起始Y坐标
+                    yPosition = height - 300; // J2模板的起始Y坐标
                     lineHeight = 17; // J2模板的行高
                 }
 
