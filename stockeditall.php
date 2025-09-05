943fd54e376031c1123a23cfb9287a2aef4141db
unknown <darrellchua118@gmail.com>
2025-09-05 11:47:25 +0800

    D111

--- stockeditall.php
+++ stockeditall.php
@@ -4302,7 +4302,7 @@
                     // Quantity (第三列) - 右对齐
                     const qtyText = outQty.toFixed(2);
                     page.drawText(qtyText, {
-                        x: getRightAlignedX(qtyText, exportSystem === 'j1' ? 365 : 365, 5),
+                        x: getRightAlignedX(qtyText, exportSystem === 'j1' ? 360 : 360, 5),
                         y: yPosition,
                         size: smallFontSize,
                         color: textColor,
