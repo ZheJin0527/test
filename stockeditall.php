7aee1fbece0350c7705b4377c9830dae7f98fd4f
unknown <darrellchua118@gmail.com>
2025-09-05 16:28:15 +0800

    D164

--- stockeditall.php
+++ stockeditall.php
@@ -4472,7 +4472,7 @@
                 }
                 
                 // 下载现有的PDF模板
-                const templateFile = exportSystem === 'j2' ? 'invoice/invoice/j2invoice.pdf' : 'invoice/invoice/j1invoice.pdf';
+                const templateFile = exportSystem === 'j2' ? 'invoice/invoice/j2invoiceMulti.pdf' : 'invoice/invoice/j1invoiceMulti.pdf';
                 const templateResponse = await fetch(templateFile);
                 if (!templateResponse.ok) {
                     throw new Error('无法加载PDF模板');
