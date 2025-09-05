24183f3799b5a2d6b0a7ceb5e3f5e1ab074eff01
kelvinloh9923 <kelvinloh9923@gmail.com>
2025-09-05 12:51:05 +0800

    3557

--- stockminimum.php
+++ stockminimum.php
@@ -18,7 +18,7 @@ session_start();
 
         body {
             font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-            background-color: #f8fafc;
+            background: linear-gradient(135deg, #ffffffff 0%, #f3ebe0ff 100%);
             color: #1a202c;
             line-height: 1.6;
         }
