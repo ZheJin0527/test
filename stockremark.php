b5e5bfa0aae8d356465101a3aab28995532fcb8b
kelvinloh9923 <kelvinloh9923@gmail.com>
2025-09-05 16:25:41 +0800

    3598

--- stockremark.php
+++ stockremark.php
@@ -268,7 +268,7 @@
         .price-variants-table th {
             background: #f8f5eb;
             color: #583e04;
-            padding: 12px;
+            padding: 120px;
             text-align: center;
             font-weight: 600;
             border-bottom: 2px solid #583e04;
@@ -984,7 +984,7 @@
                                     <thead>
                                         <tr>
                                             <th>备注编号</th>
-                                            <th>数量/重量 <span style="color: #10b981; font-weight: 600; background-color: rgba(16, 185, 129, 0.1); padding: 2px 8px; border-radius: 4px; font-size: 14px; margin-left: 65px;">总计: ${product.total_quantity}</span></th>
+                                            <th>数量/重量 <span style="color: #10b981; font-weight: 600; background-color: rgba(16, 185, 129, 0.1); padding: 2px 8px; border-radius: 4px; font-size: 14px; margin-left: 70px;">总计: ${product.total_quantity}</span></th>
                                         </tr>
                                     </thead>
                                     <tbody>`;
