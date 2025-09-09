78a565d20c8d7a773d8041ab927694c2f3beaa24
ZheJin0527 <zjwu0407@gmail.com>
2025-09-09 11:11:07 +0800

    3621

--- generatecode.php
+++ generatecode.php
@@ -314,7 +314,7 @@
         th:nth-child(5), td:nth-child(5) { width: 80px; }      /* 小名 */
         th:nth-child(6), td:nth-child(6) { width: 130px; }     /* 身份证号码 */
         th:nth-child(7), td:nth-child(7) { width: 100px; }     /* 生日 */
-        th:nth-child(8), td:nth-child(8) { width: 60px; }      /* 性别 */
+        th:nth-child(8), td:nth-child(8) { width: 40px; }      /* 性别 */
         th:nth-child(9), td:nth-child(9) { width: 80px; }      /* 国籍 */
         th:nth-child(10), td:nth-child(10) { width: 120px; }   /* 联络号码 */
         th:nth-child(11), td:nth-child(11) { width: 150px; }   /* 邮箱 */
