2d5a5a90dc3b26d11b748d4aa8e65950bed0b64e
ZheJin0527 <zjwu0407@gmail.com>
2025-09-10 17:31:05 +0800

    3741

--- stocklistall.php
+++ stocklistall.php
@@ -360,9 +360,10 @@
             border: 1px solid #462d03;
             position: sticky;
             top: 0;
-            z-index: 10;
+            z-index: 100;
             white-space: nowrap;
             min-width: 80px;
+            background-clip: padding-box;
         }
 
         /* 确保表格在容器内正确显示 */
