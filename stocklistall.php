2a8dd9bb77a410706c2c05d65209deb2724b9932
ZheJin0527 <zjwu0407@gmail.com>
2025-09-11 09:57:19 +0800

    3763

--- stocklistall.php
+++ stocklistall.php
@@ -22,7 +22,7 @@
         .container {
             max-width: 1800px;
             margin: 0 auto;
-            padding: 24px;
+            padding: 0px 24px;
         }
         
         .header {
