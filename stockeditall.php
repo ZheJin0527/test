137d4fdc3024a0fa74625b81af6d381d8757ea00
darrell-chua <darrellchua118@gmail.com>
2025-09-17 16:29:42 +0800

    D413

--- stockeditall.php
+++ stockeditall.php
@@ -5101,7 +5101,7 @@
                     const subtotalText = `RM${subtotal.toFixed(2)}`;
                     page.drawText(subtotalText, {
                         x: getRightAlignedX(subtotalText, 565, 6.5),
-                        y: height - 625, // 调整到Subtotal行
+                        y: height - 628, // 调整到Subtotal行
                         size: 11,
                         color: textColor,
                         font: boldFont,
@@ -5111,7 +5111,7 @@
                     const chargeText = `RM${charge.toFixed(2)}`;
                     page.drawText(chargeText, {
                         x: getRightAlignedX(chargeText, 565, 6.5),
-                        y: height - 638, // 调整到Charge行
+                        y: height - 639, // 调整到Charge行
                         size: 11,
                         color: textColor,
                         font: boldFont,
