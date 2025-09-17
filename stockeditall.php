fba4f60b4fe9f558b1972a507d216ece56a6900d
darrell-chua <darrellchua118@gmail.com>
2025-09-17 16:04:17 +0800

    D402

--- stockeditall.php
+++ stockeditall.php
@@ -5131,7 +5131,7 @@
                     const totalText = `RM${grandTotal.toFixed(2)}`;
                     page.drawText(totalText, {
                         x: getRightAlignedX(totalText, 565, 8),
-                        y: height - 670,
+                        y: height - 675,
                         size: fontSize,
                         color: textColor,
                         font: boldFont,
