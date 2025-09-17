9e34de5504c1a86d65e349f9e39d383858b73300
darrell-chua <darrellchua118@gmail.com>
2025-09-17 15:59:49 +0800

    D398

--- stockeditall.php
+++ stockeditall.php
@@ -5081,7 +5081,7 @@
                     // Total RM (第六列) - 右对齐
                     const totalText = total.toFixed(2);
                     page.drawText(totalText, {
-                        x: getRightAlignedX(totalText, exportSystem === 'j1' ? 500 : 500, 6),
+                        x: getRightAlignedX(totalText, exportSystem === 'j1' ? 565 : 565, 6),
                         y: yPosition,
                         size: smallFontSize,
                         color: textColor,
@@ -5131,7 +5131,7 @@
                     const totalText = `RM${grandTotal.toFixed(2)}`;
                     page.drawText(totalText, {
                         x: getRightAlignedX(totalText, 565, 8),
-                        y: height - 755,
+                        y: height - 700,
                         size: fontSize,
                         color: textColor,
                         font: boldFont,
