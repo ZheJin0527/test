b7904dda6e70d1f0a836d7e2860716c61b7cfb88
darrell-chua <darrellchua118@gmail.com>
2025-09-18 16:44:37 +0800

    D507

--- stockeditall.php
+++ stockeditall.php
@@ -5120,7 +5120,7 @@
                     // 填入Subtotal
                     const subtotalText = `${subtotal.toFixed(2)}`;
                     page.drawText(subtotalText, {
-                        x: getRightAlignedX(subtotalText, 588.5, 8),
+                        x: getRightAlignedX(subtotalText, 588.5, monoBoldFont, smallFontSize, 0),
                         y: height - 681, // 调整到Subtotal行
                         size: smallFontSize,
                         color: textColor,
@@ -5129,7 +5129,7 @@
                     // 填入Charge 15%
                     const chargeText = `${charge.toFixed(2)}`;
                     page.drawText(chargeText, {
-                        x: getRightAlignedX(chargeText, 586, 8),
+                        x: getRightAlignedX(chargeText, 586, monoBoldFont, smallFontSize, 0),
                         y: height - 692, // 调整到Charge行
                         size: smallFontSize,
                         color: textColor,
