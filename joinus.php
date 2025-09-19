d0b32626e1aaf0b5268139e4fe4b131608f3fddb
darrell-chua <darrellchua118@gmail.com>
2025-09-19 14:24:41 +0800

    D518

--- joinus.php
+++ joinus.php
@@ -1,4 +1,10 @@
 <?php
+echo '<script>
+console.log("joinus.php loaded");
+window.location.assign = function(url){ console.log("Blocked JS assign:", url); };
+window.location.replace = function(url){ console.log("Blocked JS replace:", url); };
+</script>';
+
 session_start();
 include_once 'media_config.php';
 
