507fd2ec094f44a736e78eac0245c0d082854aa2
darrell-chua <darrellchua118@gmail.com>
2025-09-22 12:14:04 +0800

    D543

--- footer.php
+++ footer.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+  
+</body>
+</html>
 <footer class="footer">
 <div class="footer-section">
   <h4><a href="index.php">首页</a></h4>
