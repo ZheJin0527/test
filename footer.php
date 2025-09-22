0013937d0b2fee9c4b0c11d75e356fb10138ce89
darrell-chua <darrellchua118@gmail.com>
2025-09-22 13:58:21 +0800

    D560

---
+++ footer.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+  
+</body>
+</html>
+<footer class="footer">
+<div class="footer-section">
+  <h4><a href="index.php">首页</a></h4>
+  <ul>
+    <li><a href="#" onclick="goToSlide(1); return false;">公司简介</a></li>
+    <li><a href="#" onclick="goToSlide(2); return false;">公司文化</a></li>
+  </ul>
+</div>
+
+<div class="footer-section">
+  <h4><a href="about.php">关于我们</a></h4>
+  <ul>
+    <li><a href="about.php#intro">集团简介</a></li>
+    <li><a href="about.php#vision">信念与方向</a></li>
+    <li><a href="about.php#values">核心价值观</a></li>
+    <li><a href="about.php#timeline-1">发展历史</a></li>
+  </ul>
+</div>
+
+<div class="footer-section">
+  <h4>旗下品牌</h4>
+  <ul>
+    <li><a href="tokyo-japanese-cuisine.php">TOKYO JAPANESE </br>CUISINE</li>
+  </ul>
+</div>
+
+<div class="footer-section">
+  <h4><a href="joinus.php">加入我们</a></h4>
+  <ul>
+    <li><a href="joinus.php">公司福利</li>
+    <li><a href="joinus.php#comphoto-container">我们的足迹</li>
+    <li><a href="joinus.php#particles">招聘的职位</li>
+    <li><a href="joinus.php#map">联系我们</a></li>        
+  </ul>
+</div>
+</footer>
+
+<button id="backToTop" onclick="scrollToTop()">&#8673;</button>
+
+<div class="footer-bottom">
+  © 2025 Kunzz Holdings Sdn. Bhd. All rights reserved.
+</div>
