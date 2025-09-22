d605de90a8fa32aeb36ebd1866e797f9be94870a
darrell-chua <darrellchua118@gmail.com>
2025-09-22 12:27:09 +0800

    D549

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ include 'header.php';
   <?php echo getMediaHtml('home_background'); ?>
 
   <div class="home-content hidden animate-on-scroll">
-    <h1 class="scale-fade-in">让空间温暖1 <span style="font-size: 1.5em;">.</span> 让团队闪光</h1>
+    <h1 class="scale-fade-in">让空间温暖 <span style="font-size: 1.5em;">.</span> 让团队闪光</h1>
     <div class="decor-line scale-fade-in"></div>
     <p class="scale-fade-in">
       我们用细节构建舒适的氛围，在积极的文化中滋养每一份热情与专注。<br />
@@ -117,8 +117,49 @@ include 'header.php';
   
   <div class="swiper-slide footer-slide">
     <section class="scroll-buffer">
-    <?php include 'footer.php'; ?>
-    </section>
+    <footer class="footer">
+    <div class="footer-section">
+      <h4><a href="index.php">首页</a></h4>
+      <ul>
+        <li><a href="#" onclick="goToSlide(1); return false;">公司简介</a></li>
+        <li><a href="#" onclick="goToSlide(2); return false;">公司文化</a></li>
+      </ul>
+    </div>
+
+    <div class="footer-section">
+      <h4><a href="about.php">关于我们</a></h4>
+      <ul>
+        <li><a href="about.php#intro">集团简介</a></li>
+        <li><a href="about.php#vision">信念与方向</a></li>
+        <li><a href="about.php#values">核心价值观</a></li>
+        <li><a href="about.php#timeline-1">发展历史</a></li>
+      </ul>
+    </div>
+
+    <div class="footer-section">
+      <h4>旗下品牌</h4>
+      <ul>
+        <li><a href="tokyo-japanese-cuisine.php">TOKYO JAPANESE </br>CUISINE</li>
+      </ul>
+    </div>
+
+    <div class="footer-section">
+      <h4><a href="joinus.php">加入我们</a></h4>
+      <ul>
+        <li><a href="joinus.php">公司福利</li>
+        <li><a href="joinus.php#comphoto-container">我们的足迹</li>
+        <li><a href="joinus.php#particles">招聘的职位</li>
+        <li><a href="joinus.php#map">联系我们</a></li>        
+      </ul>
+    </div>
+  </footer>
+
+  <button id="backToTop" onclick="scrollToTop()">&#8673;</button>
+  
+  <div class="footer-bottom">
+    © 2025 Kunzz Holdings Sdn. Bhd. All rights reserved.
+  </div>
+  </section>
   </div>
 
   
