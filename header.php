2cb9e4406b256fda2dedbf464dfe555fd1577825
darrell-chua <darrellchua118@gmail.com>
2025-09-22 17:16:25 +0800

    D589

--- header.php
+++ header.php
@@ -19,6 +19,7 @@ if (!isset($mediaConfigIncluded)) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo isset($pageTitle) ? $pageTitle : 'KUNZZ HOLDINGS'; ?></title>
     <link rel="stylesheet" href="header.css" />
+    <link rel="stylesheet" href="style.css" />
     <?php if (isset($additionalCSS)): ?>
         <?php foreach ($additionalCSS as $css): ?>
             <link rel="stylesheet" href="<?php echo $css; ?>" />
