5a5efef80ed37b6627d1a41bbf40d623a59f15ab
darrell-chua <darrellchua118@gmail.com>
2025-09-23 16:30:47 +0800

    D538

--- about.php
+++ about.php
@@ -8,7 +8,7 @@ include_once 'media_config.php';
 
 // 设置页面特定的变量
 $pageTitle = 'KUNZZ HOLDINGS';
-$additionalCSS = ['aboutanimation.css','about.css'];
+$additionalCSS = ['aboutanimation.css'];
 $showPageIndicator = true;
 $totalSlides = 5;
 
