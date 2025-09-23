1b07ae204ae615541be1b10a10f76c52fb2cd813
ZheJin0527 <zjwu0407@gmail.com>
2025-09-23 12:54:20 +0800

    4160

--- generatecode.php
+++ generatecode.php
@@ -381,7 +381,7 @@
         .table-title {
             background: #f8f5eb;
             color: #583e04;
-            padding: 20px;
+            padding: clamp(14px, 1.04vw, 20px);
             font-size: clamp(14px, 1.04vw, 20px);
             font-weight: bold;
             text-align: center;
