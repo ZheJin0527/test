5196748ec27e821b2e7cdea5ea9c4dcac6d596be
ZheJin0527 <zjwu0407@gmail.com>
2025-09-25 10:01:24 +0800

    4369

--- bgmusicupload.php
+++ bgmusicupload.php
@@ -208,7 +208,7 @@ function formatFileSize($bytes) {
         .music-section {
             background: #f8f9fa;
             border-radius: 10px;
-            padding: 30px;
+            padding: clamp(24px, 1.56vw, 30px);
             margin-bottom: 30px;
             border-left: 5px solid #583e04;
         }
