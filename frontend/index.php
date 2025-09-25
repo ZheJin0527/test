d978f0190e4b012a6cbe7ea087489baf61f71146
darrell-chua <darrellchua118@gmail.com>
2025-09-25 09:55:30 +0800

    12

--- frontend/index.php
+++ frontend/index.php
@@ -15,7 +15,7 @@ if (isset($_SESSION['user_id']) || (isset($_COOKIE['user_id']) && isset($_COOKIE
 
 // 设置页面特定的变量
 $pageTitle = 'KUNZZ HOLDINGS';
-$additionalCSS = ['css/animation.css', 'css/responsive.css'];
+$additionalCSS = ['css/frontend-main.css', 'css/animation.css', 'css/responsive.css'];
 $showPageIndicator = true;
 $totalSlides = 4;
 
