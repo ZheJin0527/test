bf5d63e4c0ce056822ffe306c1c7279d6eadc69b
darrell-chua <darrellchua118@gmail.com>
2025-09-25 17:18:20 +0800

    R69

--- about.php
+++ about.php
@@ -177,54 +177,8 @@ $timelineData = getTimelineConfig();
     </section>
   </div>
 
-  <div class="swiper-slide footer-slide">
-    <section class="scroll-buffer">
-    <footer class="footer">
-    <div class="footer-section">
-      <h4><a href="index.php">首页</a></h4>
-      <ul>
-        <li><a href="index.php#comprofile">公司简介</a></li>
-        <li><a href="index.php#culture">公司文化</a></li>
-      </ul>
-    </div>
+<?php include 'footer.php'; ?>
 
-    <div class="footer-section">
-      <h4><a href="about.php">关于我们</a></h4>
-      <ul>
-        <li><a href="#" onclick="goToSlide(0); return false;">集团简介</a></li>
-        <li><a href="#" onclick="goToSlide(1); return false;">信念与方向</a></li>
-        <li><a href="#" onclick="goToSlide(2); return false;">核心价值观</a></li>
-        <li><a href="#" onclick="goToSlide(3); return false;">发展历史</a></li>
-      </ul>
-    </div>
-
-    <div class="footer-section">
-      <h4>旗下品牌</h4>
-      <ul>
-        <li><a href="tokyo-japanese-cuisine.php">TOKYO JAPANESE </br>CUISINE</li>
-      </ul>
-    </div>
-
-    <div class="footer-section">
-      <h4><a href="joinus.php">加入我们</a></h4>
-      <ul>
-        <li><a href="joinus.php">公司福利</li>
-        <li><a href="joinus.php#comphoto-container">我们的足迹</li>
-        <li><a href="joinus.php#particles">招聘的职位</li>
-        <li><a href="joinus.php#map">联系我们</a></li>        
-      </ul>
-    </div>
-  </footer>
-
-  <button id="backToTop" onclick="scrollToTop()">&#8673;</button>
-  
-  <div class="footer-bottom">
-    © 2025 Kunzz Holdings Sdn. Bhd. All rights reserved.
-  </div>
-  </section>
-  </div>
-
-  
   </div> <!-- 关闭 swiper-wrapper -->
 </div> <!-- 关闭 swiper -->
 
@@ -247,6 +201,7 @@ $timelineData = getTimelineConfig();
   
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="app.js"></script>
+<script src="header.js"></script>
 <script>
         const hamburger = document.getElementById('hamburger');
         const navMenu = document.getElementById('navMenu');
