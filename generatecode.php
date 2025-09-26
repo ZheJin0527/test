27bb3d87824586bd737ebd2d7d24959bc6644ad0
ZheJin0527 <zjwu0407@gmail.com>
2025-09-26 11:08:19 +0800

    4490

--- generatecode.php
+++ generatecode.php
@@ -451,7 +451,7 @@
         }
 
         td {
-            padding: clamp(2px, 0.42vw, 8px) 6px;
+            padding: clamp(0px, 0.31vw, 6px) 6px;
             font-size: clamp(8px, 0.74vw, 14px);
             font-weight: 500;
             border: 1px solid #d1d5db;
