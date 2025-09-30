7f1c230c108a4f2f6d205c3dedad56eb27763497
ZheJin0527 <zjwu0407@gmail.com>
2025-09-30 12:32:31 +0800

    4658

--- stocklistall.php
+++ stocklistall.php
@@ -133,7 +133,7 @@
         .dropdown-item.active {
             background-color: #f99e00;
             color: white;
-            border-radius: 4px;
+            border-top: 4px;
         }
 
         .back-button {
