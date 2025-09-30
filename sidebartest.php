af9539c29c83dec4d99200df2c8be5c805b3fb6e
ZheJin0527 <zjwu0407@gmail.com>
2025-09-30 17:22:57 +0800

    4724

--- sidebartest.php
+++ sidebartest.php
@@ -264,7 +264,7 @@ if (isset($_SESSION['user_id'])) {
     transition: all 0.2s ease;
     cursor: pointer;
     position: relative;
-    border-left: 1px solid #ff5c00;
+    border-left: 2px solid #ff5c00;
 }
 
 .informationmenu-item:hover {
