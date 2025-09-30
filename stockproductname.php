4334407dbd7aeb6aa806ec89905a929069ceff78
ZheJin0527 <zjwu0407@gmail.com>
2025-09-30 12:31:07 +0800

    4657

--- stockproductname.php
+++ stockproductname.php
@@ -114,7 +114,7 @@ if (isset($_SESSION['user_id'])) {
             right: 0;
             background: white;
             border: 2px solid #000000ff;
-            border-radius: 8px;
+            border-bottom: 8px;
             box-shadow: 0 8px 24px rgba(88, 62, 4, 0.2);
             width: 100%;
             z-index: 10000;
@@ -148,7 +148,7 @@ if (isset($_SESSION['user_id'])) {
 
         .selector-dropdown .dropdown-item.active {
             background-color: #f99e00 !important;
-            border-radius: 4px !important;
+            border-bottom: 4px !important;
             color: white !important;
         }
 
