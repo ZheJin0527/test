986f8ec97caab513aad774f4351ee8703c92160c
ZheJin0527 <zjwu0407@gmail.com>
2025-09-30 15:09:42 +0800

    4686

--- tokyopage5upload.php
+++ tokyopage5upload.php
@@ -251,6 +251,7 @@ $currentConfig = getTokyoLocationConfig();
 
         .btn-add:hover {
             background: #218838;
+            box-shadow: 0 5px 15px rgba(8, 94, 0, 0.3);
         }
         
         .alert {
