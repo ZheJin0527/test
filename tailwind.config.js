4f0a7e455dd948c0b50a560832d718525adccf8d
darrell-chua <darrellchua118@gmail.com>
2025-10-15 16:48:31 +0800

    47

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./frontend/public/**/*.{php,html,js}",
+    "./src/**/*.{css,js,php,html}",
+    "./*.php"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
